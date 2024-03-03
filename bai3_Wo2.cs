6ce01e98e2a3953baee912b47b3f86a287333454
zorozomeo07 <125963795+zorozomeo07@users.noreply.github.com>
2024-03-03 18:27:12 +0800

    phv
    phv

---
+++ bai3_Wo2.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bai1_W01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+            
+        {
+            Console.WriteLine("--- Chương trình quy đổi tỉ giá tiền tệ ---");
+            Console.WriteLine("Nhập giá trị số tiền USD");
+            double usd = double.Parse(Console.ReadLine());
+            // ct quy đổi
+            double vnd = usd * 23000;
+            Console.WriteLine("Giá trị quy đổi từ "+usd+" usd sang vnd là : "+vnd+" vnd");
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
