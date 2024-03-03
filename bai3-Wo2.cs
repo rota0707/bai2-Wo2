dc1000d3ab7ca6e8e557df959d8660e8bf9f647e
zorozomeo07 <125963795+zorozomeo07@users.noreply.github.com>
2024-03-03 18:21:35 +0800

    phv
    phv

---
+++ bai3-Wo2.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bai1_W01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+            
+        {
+            Console.WriteLine("Enter your name");
+            String name = Console.ReadLine();
+            Console.WriteLine("Hello :" + name);
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
